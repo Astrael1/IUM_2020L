01322d63e5254172f03fb0ef11522f2597088d98
Szymon Małolepszy <szymon8999@wp.pl>
2020-05-03 21:05:19 +0200

    some preprocessing

--- program.py
+++ program.py
@@ -6,9 +6,25 @@ product_data = pd.read_json('resources/products.jsonl', lines=True)
 session_data = pd.read_json('resources/sessions.jsonl', lines=True)
 users_data = pd.read_json('resources/users.jsonl', lines=True)
 
+# find products with price lower or equal 0 and greater than 1000000
+incorrect_products = product_data[ (product_data["price"] <= 0) | (product_data["price"] > 1000000) ].index
+# find values of their 'product_id' column
+incorrect_products_ids = product_data["product_id"].iloc[incorrect_products].unique()
+
+# drop incorrect products
+product_data.drop(incorrect_products, inplace=True)
+
 # change strings with categories into list of categories
 product_data["category_path"] = product_data["category_path"].apply(lambda x: x.split(';'))
 
-print(product_data[["product_id", "product_name", "category_path"]].head(10))
-print(product_data["category_path"].dtypes)
+# drop rows with null value of columns 'user_id' or 'product_id'
+session_data.dropna(subset=['user_id', 'product_id'], inplace=True)
+
+# drop rows with products that are incorrect
+incorrect_sessions = session_data[ session_data["product_id"].isin(incorrect_products_ids)].index
+session_data.drop(incorrect_sessions, inplace=True)
+
+# set amount of rows print
+pd.set_option('display.max_rows', 100)
+
 
