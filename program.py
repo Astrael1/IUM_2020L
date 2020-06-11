94618c5fd46bc340a62c1182d2082d9a2f2b7f21
astrael <szymon8999@wp.pl>
2020-06-11 22:14:06 +0200

    removed products with incorrect names

--- program.py
+++ program.py
@@ -6,14 +6,18 @@ product_data = pd.read_json('resources/products.jsonl', lines=True)
 session_data = pd.read_json('resources/sessions.jsonl', lines=True)
 users_data = pd.read_json('resources/users.jsonl', lines=True)
 
-# find products with price lower or equal 0 and greater than 1000000
-incorrect_products = product_data[ (product_data["price"] <= 0) | (product_data["price"] > 1000000) ].index
+# find products with price lower or equal 0 and greater than 1000000 and those which have '#','&' or ';' in name
+incorrect_products = product_data[ 
+    (product_data["price"] <= 0) | 
+    (product_data["price"] > 1000000) |
+    (product_data['product_name'].str.contains('#|;|&', regex=True)) ].index
+
 # find values of their 'product_id' column
 incorrect_products_ids = product_data["product_id"].iloc[incorrect_products].unique()
 
+
 # drop incorrect products
 product_data.drop(incorrect_products, inplace=True)
-
 # change strings with categories into list of categories
 product_data["category_path"] = product_data["category_path"].apply(lambda x: x.split(';'))
 
@@ -21,8 +25,12 @@ product_data["category_path"] = product_data["category_path"].apply(lambda x: x.
 session_data.dropna(subset=['user_id', 'product_id'], inplace=True)
 session_data.drop(columns=['purchase_id'], inplace=True)
 
+# replace floats with ints
+session_data.user_id = session_data.user_id.astype(int)
+session_data.product_id = session_data.product_id.astype(int)
 # drop rows with products that are incorrect
 incorrect_sessions = session_data[ session_data["product_id"].isin(incorrect_products_ids)].index
+print(len(incorrect_sessions))
 session_data.drop(incorrect_sessions, inplace=True)
 
 users_data.drop(columns=['name', 'street'], inplace=True)
@@ -30,6 +38,9 @@ users_data.drop(columns=['name', 'street'], inplace=True)
 # set amount of rows print
 pd.set_option('display.max_rows', 100)
 
-import pdb; pdb.set_trace()
+
+
+
+
 
 
