2f939572baf270d088d9ec9bd7c53c6b1aa19763
astrael <szymon8999@wp.pl>
2020-06-19 15:28:23 +0200

    need to refactor reading products

--- program.py
+++ program.py
@@ -1,5 +1,6 @@
 #/usr/bin/python3
 import pandas as pd
+import numpy as np
 import argparse
 
 product_data = pd.read_json('resources/products.jsonl', lines=True)
@@ -37,6 +38,76 @@ users_data.drop(columns=['name', 'street'], inplace=True)
 
 # set amount of rows print
 pd.set_option('display.max_rows', 100)
+all_session_ids = session_data.session_id.unique()
+
+def getSessionOfId(session_data, id):
+    return session_data[session_data.session_id == id]
+
+def getSuccessfullSessions(session_data, session_ids):
+    return [id for id in session_ids if (getSessionOfId(session_data, id).event_type == 'BUY_PRODUCT').any() ]
+
+# print(session_data[session_data.session_id.isin(getSuccessfullSessions(session_data, all_session_ids))])
+
+def getListOfCategories(product_data):
+    a = set()
+    for categories in product_data.category_path:
+        for item in categories:
+            a.add(item)
+    return sorted(list(a))
+
+
+
+groupped_sessions = session_data.groupby('session_id')
+
+def getSessionAttributes(session, products):
+
+    s_id = session['session_id'].unique()[0]
+  
+    timestamps = session.iloc[[0,-1]].timestamp.values
+    duration = (timestamps[1] - timestamps[0])/ np.timedelta64(1,'s')
+
+    discount = session[ 'offered_discount'].values
+
+    product_ids = group['product_id']
+    viewed_products_data = products[products.product_id.isin(product_ids)][['price']]
+    try:
+        viewed_products_data.insert(0, 'offered_discount', discount, True)
+    except ValueError:
+        print("ERROR", viewed_products_data)
+        import pdb; pdb.set_trace()
+    viewed_products_data.insert(1, 'discount_value', 
+        viewed_products_data.price * viewed_products_data.offered_discount, True)
+
+    mean_values = viewed_products_data.mean()
+
+    avg_price = mean_values.price
+    avg_discount_percent = mean_values.offered_discount
+    avg_discount_value = mean_values.discount_value
+
+    sum_values = viewed_products_data.sum()
+    total_discount_percent = sum_values.offered_discount
+    total_discount_value = sum_values.discount_value
+    
+    result = {
+            'id': s_id,
+            'duration': duration,
+            'avg_price': avg_price,
+            'avg_discount_percent': avg_discount_percent,
+            'total_discount_percent': total_discount_percent,
+            'avg_dicount_value': avg_discount_value,
+            'total_discount_value': total_discount_value
+        }
+    return result
+
+data = []
+
+for name, group in groupped_sessions:
+    data.append(getSessionAttributes(group, product_data))
+
+sessiondf = pd.DataFrame(data)
+    
+
+import pdb; pdb.set_trace()
 
 
 
