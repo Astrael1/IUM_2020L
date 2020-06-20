c3f8aa6f7b816dc8e6d15c258dd5236885a12262
astrael <szymon8999@wp.pl>
2020-06-20 18:39:48 +0200

    some changes

--- preprocess_basic.py
+++ preprocess_basic.py
@@ -41,78 +41,81 @@ users_data.drop(columns=['name', 'street'], inplace=True)
 pd.set_option('display.max_rows', 100)
 all_session_ids = session_data.session_id.unique()
 
-
-groupped_sessions = session_data.groupby('session_id')
-
-
-def getSessionAttributes(session, products):
-    s_id = session['session_id'].unique()[0]
-
-    timestamps = session.iloc[[0, -1]].timestamp.values
-    duration = (timestamps[1] - timestamps[0]) / np.timedelta64(1, 's')
-
-    event = session.iloc[-1].event_type
-    isSuccessfull = True if event == 'BUY_PRODUCT' else False
-
-    discount = session['offered_discount'].values
-
-    product_ids = group['product_id']
-    viewed_products_data = products.loc[product_ids, ['product_name', 'price']]
-    viewed_products_data.insert(1, 'offered_discount', discount, True)
-    viewed_products_data.insert(2, 'discount_value',
-                                viewed_products_data.price * viewed_products_data.offered_discount, True)
-
-    mean_values = viewed_products_data.mean()
-
-    avg_price = mean_values.price
-    avg_discount_percent = mean_values.offered_discount
-    avg_discount_value = mean_values.discount_value
-
-    sum_values = viewed_products_data.sum()
-    total_discount_percent = sum_values.offered_discount
-    total_discount_value = sum_values.discount_value
-
-    result = {
-        'id': s_id,
-        'duration': duration,
-        'avg_price': avg_price,
-        'avg_discount_percent': avg_discount_percent,
-        'total_discount_percent': total_discount_percent,
-        'avg_discount_value': avg_discount_value,
-        'total_discount_value': total_discount_value,
-        'success': isSuccessfull
-    }
-    return result
+def make_training_set(session_data, product_data):
+    groupped_sessions = session_data.groupby('session_id')
 
 
-data = []
-for name, group in groupped_sessions:
-    data.append(getSessionAttributes(group, product_data))
+    def getSessionAttributes(session, products):
+        s_id = session['session_id'].unique()[0]
 
-sessiondf = pd.DataFrame(data)
-sessiondf.set_index('id', inplace=True)
+        timestamps = session.iloc[[0, -1]].timestamp.values
+        duration = (timestamps[1] - timestamps[0]) / np.timedelta64(1, 's')
+
+        event = session.iloc[-1].event_type
+        isSuccessfull = True if event == 'BUY_PRODUCT' else False
+
+        discount = session['offered_discount'].values
+
+        product_ids = group['product_id']
+        viewed_products_data = products.loc[product_ids, ['product_name', 'price']]
+        viewed_products_data.insert(1, 'offered_discount', discount, True)
+        viewed_products_data.insert(2, 'discount_value',
+                                    viewed_products_data.price * viewed_products_data.offered_discount, True)
+
+        mean_values = viewed_products_data.mean()
+
+        avg_price = mean_values.price
+        avg_discount_percent = mean_values.offered_discount
+        avg_discount_value = mean_values.discount_value
+
+        sum_values = viewed_products_data.sum()
+        total_discount_percent = sum_values.offered_discount
+        total_discount_value = sum_values.discount_value
+
+        result = {
+            'id': s_id,
+            'duration': duration,
+            'avg_price': avg_price,
+            'avg_discount_percent': avg_discount_percent,
+            'total_discount_percent': total_discount_percent,
+            'avg_discount_value': avg_discount_value,
+            'total_discount_value': total_discount_value,
+            'success': isSuccessfull
+        }
+        return result
 
 
-def simplifyColumn(df, column):
-    df.loc[df[column] > 0, column] = df.loc[df[column] > 0, column].apply(np.log10).apply(np.floor)
+    data = []
+    for name, group in groupped_sessions:
+        data.append(getSessionAttributes(group, product_data))
+
+    sessiondf = pd.DataFrame(data)
+    sessiondf.set_index('id', inplace=True)
 
 
-sessiondf_p = sessiondf.copy()
-for column in sessiondf_p.columns[:-1]:
-    simplifyColumn(sessiondf_p, column)
+    def simplifyColumn(df, column):
+        df.loc[df[column] > 0, column] = df.loc[df[column] > 0, column].apply(np.log10).apply(np.floor)
 
-grouped_p = sessiondf_p.groupby(
-    ['duration', 'avg_price', 'avg_discount_percent', 'total_discount_percent', 'avg_discount_value',
-     'total_discount_value'])
 
-sessiondf['probability'] = np.nan
+    sessiondf_p = sessiondf.copy()
+    for column in sessiondf_p.columns[:-1]:
+        simplifyColumn(sessiondf_p, column)
 
-for name, group in grouped_p:
-    result = group['success'].sum() / len(group)
-    sessiondf.loc[group.index, 'probability'] = result
-    print(name, result)
+    grouped_p = sessiondf_p.groupby(
+        ['duration', 'avg_price', 'avg_discount_percent', 'total_discount_percent', 'avg_discount_value',
+        'total_discount_value'])
 
-sessiondf.drop(inplace=True, columns=['success'])
+    sessiondf['probability'] = np.nan
+
+    for name, group in grouped_p:
+        result = group['success'].sum() / len(group)
+        sessiondf.loc[group.index, 'probability'] = result
+        print(name, result)
+
+    sessiondf.drop(inplace=True, columns=['success'])
+    return sessiondf
+
+sessiondf = make_training_set(session_data, product_data)
 
 corr = sessiondf.corr()
 
@@ -124,3 +127,4 @@ sns.heatmap(corr, mask=mask, vmax=.3, center=0,
             square=True, linewidths=.5, cbar_kws={"shrink": .5})
 
 sessiondf.to_csv(path_or_buf='session_preprocessed.csv')
+product_data.to_csv(path_or_buf='products_preprocessed.csv')
